42e679b3bcafe359525d4df0ca8584021b9bbd3a
Camila Gabriele <camilagabriele@outlook.com.ar>
2025-11-23 13:26:09 +0000

    cambios finales

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
-import "bootstrap";
-import "./style.css";
+const choises = ["rock", "paper", "scissors", "lizard", "spock"]
+const rules = {
+  rock: ['scissors', "lizard"],
+  paper: ["rock", "spock"],
+  scissors: ["paper", "lizard"],
+  lizard: ["spock", "paper"],
+  spock: ["scissors", "rock"],
+}
 
+function choisesConsole(arr) {
+  const choiseAleatoria = Math.floor(Math.random() * arr.length)
+  return arr[choiseAleatoria]
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const ganaUsuario = (opciones, eleccionConsole) => {
+  for (const opcion of opciones) {
+    if (opcion === eleccionConsole) {
+      return true
+    }
+    return false
+  }
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function jugar(userChoises) {
+  const respuestaConsole = choisesConsole(choises)
+  console.log("Usuario escogió :", userChoises);
+  console.log("Console escogió:", respuestaConsole);
+
+  if (userChoises === respuestaConsole) {
+    console.log("Empate!!");
+  } else if (ganaUsuario(rules[userChoises], respuestaConsole)) {
+    console.log("Your winner =)");
+  } else {
+    console.log("You lost =/");
+  }
+}
+
+jugar("rock")
